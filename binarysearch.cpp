4ea0614c89b15515df6d00e6ecf7bb11c87cef06
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-07 19:32:37 +0700

    membuat input data

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,21 @@ using namespace std;
 int element[10];
 int npanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> npanjang;
+        if (npanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+
+        }
+    }
+}
